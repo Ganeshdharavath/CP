a594c5d4984aa1476afb081be7091c5c7b7f4938
Ganeshdharavath <163624093+Ganeshdharavath@users.noreply.github.com>
2024-03-26 15:31:40 +0530

    Create partition list.py

---
+++ partition list.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        d1=c1=ListNode(0)
+        d2=c2=ListNode(0)
+        while head:
+            if head.val<x:
+                d1.next=head
+                d1=d1.next
+
+            else:
+                d2.next=head
+                d2=d2.next    
+
+            head=head.next
+
+        d2.next=None
+        d1.next=c2.next
+        return c1.next    
